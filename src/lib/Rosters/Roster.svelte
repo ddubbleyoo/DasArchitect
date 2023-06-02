b7150ebf18a668bd713a238a18e584f66688df0c
ddubbleyoo <134633769+ddubbleyoo@users.noreply.github.com>
2023-06-01 20:25:27 -0700

    Update Roster.svelte

--- src/lib/Rosters/Roster.svelte
+++ src/lib/Rosters/Roster.svelte
@@ -39,60 +39,64 @@
   /* ...existing styles... */
 </style>
 
+
 <div class="team">
-  <DataTable class="teamInner" table$aria-label="Team Name" style="width: {innerWidth * 0.95 > 380 ? 380 : innerWidth * 0.95}px;">
-    <Head>
-      <Row>
-        <Cell colspan=4 class="r_{division} clickable">
-          <!-- ...existing code... -->
+	<DataTable class="teamInner" table$aria-label="Team Name" style="width: {innerWidth * 0.95 > 380 ? 380 : innerWidth * 0.95}px;">
+		<Head> <!-- Team name and additional headers -->
+			<Row>
+				<Cell colspan=1 class="r_{division} clickable">
+					<h3 on:click={() => gotoManager({leagueTeamManagers, rosterID: roster.roster_id})}>
+						<img alt="team avatar" class="teamAvatar" src="{team ? team.avatar : 'https://sleepercdn.com/images/v2/icons/player_default.webp'}" />
+						{team?.name ? team.name : 'No Manager'}
+					</h3>
 
-          <Cell class="r_{division}">Year Signed</Cell>
-          <Cell class="r_{division}">Contract Length</Cell>
-          <Cell class="r_{division}">Salary</Cell>
-          <Cell class="r_{division}">Designation</Cell>
-        </Row>
-      </Head>
-      <Body>
-        <!-- ...existing code... -->
-        {#each finalStarters as starter}
-          <Row>
-            <Cell>{starter.name}</Cell>
-            <Cell>{starter.poss}</Cell>
-            <Cell>{starter.team}</Cell>
-            <Cell>{starter.slot}</Cell>
-            <!-- Add the new columns here -->
-            <Cell>{starter.yearSigned}</Cell>
-            <Cell>{starter.contractLength}</Cell>
-            <Cell>{starter.salary}</Cell>
-            <Cell>{starter.designation}</Cell>
-            <!-- ...existing code... -->
-          </Row>
-        {/each}
-        <!-- ...existing code... -->
-      </Body>
-    </DataTable>
-    <div class="rosterBench" style="max-height: {selected}px;">
-      <DataTable class="teamInner" style="width: 380px;">
-        <Body class="bench">
-          <!-- ...existing code... -->
-        </Body>
-      </DataTable>
-    </div>
-  </DataTable>
+					<div class="record">
+						{#each record as result}
+							<img alt="match result" class="result" src="./{result}.png" />
+						{/each}
+					</div>
+				</Cell>
+				<Cell colspan=1 class="r_{division}"><h5>POS</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>FACE</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>TEAM</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>PLAYER</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>SIGNED</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>Length</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>Salary</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>Designation</h5></Cell>
+			</Row>
+		</Head>
+		<Body>
+			<!-- Starters -->
+			{#each finalStarters as starter}
+				<RosterRow player={starter} />
+			{/each}
+			<Row class="interactive" on:click={toggleSelected}>
+				<Cell colspan=9 class="{division}"><h5><Icon class="material-icons icon">king_bed</Icon> Bench <span class="italic">({status})</span></h5></Cell>
+			</Row>
+		</Body>
+	</DataTable>
+	<div class="rosterBench" style="max-height: {selected}">
+		<DataTable class="teamInner" style="width: 380px">
+			<Body class="bench">
+				<!-- Bench -->
+				{#each finalBench as bench}
+					<RosterRow player={bench} />
+				{/each}
+
+				<!-- IR -->
+				{#if finalIR}
+					<Row>
+						<Cell colspan=9><h5><Icon class="material-icons icon">healing</Icon> Injured Reserve</h5></Cell>
+					</Row>
+					{#each finalIR as ir}
+						<RosterRow player={ir} />
+					{/each}
+				{/if}
+				<Row class="interactive" on:click={toggleSelected}>
+					<Cell colspan=9 class="{division}"><h5><Icon class="material-icons icon">close_fullscreen</Icon> Close Bench</h5></Cell>
+				</Row>
+			</Body>
+		</DataTable>
+	</div>
 </div>
-
-<script>
-  // ...existing code...
-
-  // Populate the new columns with delimited data from Player Nickname
-  finalStarters = finalStarters.map(starter => {
-    const playerNicknameParts = starter.playerNickname.split(' ');
-    return {
-      ...starter,
-      yearSigned: playerNicknameParts[0] || '',
-      contractLength: playerNicknameParts[1] || '',
-      salary: playerNicknameParts[2] || '',
-      designation: playerNicknameParts[3] || ''
-    };
-  });
-</script>
