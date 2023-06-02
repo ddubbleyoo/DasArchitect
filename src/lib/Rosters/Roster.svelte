9d0eaaba2488eb982325be6687e4bbb406a1ac60
ddubbleyoo <134633769+ddubbleyoo@users.noreply.github.com>
2023-06-01 20:17:46 -0700

    Update Roster.svelte

--- src/lib/Rosters/Roster.svelte
+++ src/lib/Rosters/Roster.svelte
@@ -258,10 +258,10 @@
 </style>
 
 <div class="team">
-	<DataTable class="teamInner" table$aria-label="Team Name" style="width: {innerWidth * 0.95 > 380 ? 380 : innerWidth * 0.95}px;" >
-		<Head> <!-- Team name  -->
+	<DataTable class="teamInner" table$aria-label="Team Name" style="width: {innerWidth * 0.95 > 380 ? 380 : innerWidth * 0.95}px;">
+		<Head> <!-- Team name and additional headers -->
 			<Row>
-				<Cell colspan=4 class="r_{division} clickable">
+				<Cell colspan=1 class="r_{division} clickable">
 					<h3 on:click={() => gotoManager({leagueTeamManagers, rosterID: roster.roster_id})}>
 						<img alt="team avatar" class="teamAvatar" src="{team ? team.avatar : 'https://sleepercdn.com/images/v2/icons/player_default.webp'}" />
 						{team?.name ? team.name : 'No Manager'}
@@ -273,37 +273,45 @@
 						{/each}
 					</div>
 				</Cell>
+				<Cell colspan=1 class="r_{division}"><h5>POS</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>FACE</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>TEAM</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>PLAYER</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>SIGNED</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>Length</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>Salary</h5></Cell>
+				<Cell colspan=1 class="r_{division}"><h5>Designation</h5></Cell>
 			</Row>
 		</Head>
 		<Body>
-			<!-- 	Starters	 -->
+			<!-- Starters -->
 			{#each finalStarters as starter}
 				<RosterRow player={starter} />
 			{/each}
 			<Row class="interactive" on:click={toggleSelected}>
-				<Cell colspan=4 class="{division}"><h5><Icon class="material-icons icon">king_bed</Icon> Bench <span class="italic">({status})</span></h5></Cell>
+				<Cell colspan=9 class="{division}"><h5><Icon class="material-icons icon">king_bed</Icon> Bench <span class="italic">({status})</span></h5></Cell>
 			</Row>
 		</Body>
 	</DataTable>
 	<div class="rosterBench" style="max-height: {selected}">
-		<DataTable class="teamInner" style="width: 380px" >
+		<DataTable class="teamInner" style="width: 380px">
 			<Body class="bench">
-				<!-- 	Bench	 -->
+				<!-- Bench -->
 				{#each finalBench as bench}
 					<RosterRow player={bench} />
 				{/each}
-				
-				<!-- 	IR	 -->
+
+				<!-- IR -->
 				{#if finalIR}
 					<Row>
-					<Cell colspan=4 ><h5><Icon class="material-icons icon">healing</Icon> Injured Reserve</h5></Cell>
+						<Cell colspan=9><h5><Icon class="material-icons icon">healing</Icon> Injured Reserve</h5></Cell>
 					</Row>
 					{#each finalIR as ir}
 						<RosterRow player={ir} />
 					{/each}
 				{/if}
 				<Row class="interactive" on:click={toggleSelected}>
-					<Cell colspan=4 class="{division}"><h5><Icon class="material-icons icon">close_fullscreen</Icon>Close Bench</h5></Cell>
+					<Cell colspan=9 class="{division}"><h5><Icon class="material-icons icon">close_fullscreen</Icon> Close Bench</h5></Cell>
 				</Row>
 			</Body>
 		</DataTable>
