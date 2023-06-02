f701e528142478d8144b9d357bbf4121baaa5f9c
ddubbleyoo <134633769+ddubbleyoo@users.noreply.github.com>
2023-06-01 18:59:51 -0700

    Update Roster.svelte

--- src/lib/Rosters/Roster.svelte
+++ src/lib/Rosters/Roster.svelte
@@ -258,54 +258,81 @@
 </style>
 
 <div class="team">
-	<DataTable class="teamInner" table$aria-label="Team Name" style="width: {innerWidth * 0.95 > 380 ? 380 : innerWidth * 0.95}px;" >
-		<Head> <!-- Team name  -->
-			<Row>
-				<Cell colspan=4 class="r_{division} clickable">
-					<h3 on:click={() => gotoManager({leagueTeamManagers, rosterID: roster.roster_id})}>
-						<img alt="team avatar" class="teamAvatar" src="{team ? team.avatar : 'https://sleepercdn.com/images/v2/icons/player_default.webp'}" />
-						{team?.name ? team.name : 'No Manager'}
-					</h3>
+	<DataTable class="teamInner" table$aria-label="Team Name" style="width: {innerWidth * 0.95 > 380 ? 380 : innerWidth * 0.95}px;">
+  <Head> <!-- Team name  -->
+    <Row>
+      <Cell colspan=4 class="r_{division} clickable">
+        <h3 on:click={() => gotoManager({leagueTeamManagers, rosterID: roster.roster_id})}>
+          <img alt="team avatar" class="teamAvatar" src="{team ? team.avatar : 'https://sleepercdn.com/images/v2/icons/player_default.webp'}" />
+          {team?.name ? team.name : 'No Manager'}
+        </h3>
 
-					<div class="record">
-						{#each record as result}
-							<img alt="match result" class="result" src="./{result}.png" />
-						{/each}
-					</div>
-				</Cell>
-			</Row>
-		</Head>
-		<Body>
-			<!-- 	Starters	 -->
-			{#each finalStarters as starter}
-				<RosterRow player={starter} />
-			{/each}
-			<Row class="interactive" on:click={toggleSelected}>
-				<Cell colspan=4 class="{division}"><h5><Icon class="material-icons icon">king_bed</Icon> Bench <span class="italic">({status})</span></h5></Cell>
-			</Row>
-		</Body>
-	</DataTable>
-	<div class="rosterBench" style="max-height: {selected}">
-		<DataTable class="teamInner" style="width: 380px" >
-			<Body class="bench">
-				<!-- 	Bench	 -->
-				{#each finalBench as bench}
-					<RosterRow player={bench} />
-				{/each}
-				
-				<!-- 	IR	 -->
-				{#if finalIR}
-					<Row>
-					<Cell colspan=4 ><h5><Icon class="material-icons icon">healing</Icon> Injured Reserve</h5></Cell>
-					</Row>
-					{#each finalIR as ir}
-						<RosterRow player={ir} />
-					{/each}
-				{/if}
-				<Row class="interactive" on:click={toggleSelected}>
-					<Cell colspan=4 class="{division}"><h5><Icon class="material-icons icon">close_fullscreen</Icon>Close Bench</h5></Cell>
-				</Row>
-			</Body>
-		</DataTable>
-	</div>
-</div>
+        <div class="record">
+          {#each record as result}
+            <img alt="match result" class="result" src="./{result}.png" />
+          {/each}
+        </div>
+      </Cell>
+      <Cell class="r_{division}">Year Signed</Cell>
+      <Cell class="r_{division}">Contract Length</Cell>
+      <Cell class="r_{division}">Salary</Cell>
+      <Cell class="r_{division}">Special Designation</Cell>
+    </Row>
+  </Head>
+  <Body>
+    <!-- Starters -->
+    {#each finalStarters as starter}
+      <Row>
+        <Cell>{starter.name}</Cell>
+        <Cell>{starter.poss}</Cell>
+        <Cell>{starter.team}</Cell>
+        <Cell>{starter.slot}</Cell>
+        {#if starter.name.includes(' ')}
+          {#each starter.name.split(' ') as nickname}
+            <Cell>{nickname}</Cell>
+          {/each}
+        {else}
+          <Cell></Cell>
+          <Cell></Cell>
+          <Cell></Cell>
+        {/if}
+      </Row>
+    {/each}
+    <Row class="interactive" on:click={toggleSelected}>
+      <Cell colspan=4 class="{division}"><h5><Icon class="material-icons icon">king_bed</Icon> Bench <span class="italic">({status})</span></h5></Cell>
+    </Row>
+  </Body>
+</DataTable>
+<div class="rosterBench" style="max-height: {selected}">
+  <DataTable class="teamInner" style="width: 380px">
+    <Body class="bench">
+      <!-- Bench -->
+      {#each finalBench as bench}
+        <Row>
+          <Cell>{bench.name}</Cell>
+          <Cell>{bench.poss}</Cell>
+          <Cell>{bench.team}</Cell>
+          <Cell>{bench.slot}</Cell>
+          {#if bench.name.includes(' ')}
+            {#each bench.name.split(' ') as nickname}
+              <Cell>{nickname}</Cell>
+            {/each}
+          {else}
+            <Cell></Cell>
+            <Cell></Cell>
+            <Cell></Cell>
+          {/if}
+        </Row>
+      {/each}
+      
+      <!-- IR -->
+      {#if finalIR}
+        <Row>
+          <Cell colspan=4><h5><Icon class="material-icons icon">healing</Icon> Injured Reserve</h5></Cell>
+        </Row>
+        {#each finalIR as ir}
+          <Row>
+            <Cell>{ir.name}</Cell>
+            <Cell>{ir.poss}</Cell>
+            <Cell>{ir.team}</Cell>
+            <Cell>{ir.slot}</Cell
